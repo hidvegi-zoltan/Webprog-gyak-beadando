887960cf70404e789a8e0d55dd0b183c020967c5
hidvegi-zoltan <hdvg.zoli@gmail.com>
2026-05-09 17:15:09 +0200

    Final, adatbazis kapcsolat szandekosan nem tartalmazza a belépési adatokat, feltoltott verzio igen

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -24,10 +24,10 @@ $oldalak = [
     'kapcsolat'  => ['fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat',    'menun' => [1,1]],
     'uzenetek'   => ['fajl' => 'uzenetek',  'szoveg' => 'Üzenetek',     'menun' => [0,1]],
     'belepes'    => ['fajl' => 'belepes',   'szoveg' => 'Belépés',      'menun' => [1,0]],
-    'belep'      => ['fajl' => 'belep',     'szoveg' => '',             'menun' => [0,0]],
     'regisztral' => ['fajl' => 'regisztral','szoveg' => '',             'menun' => [0,0]],
     'kepek' => ['fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => [1,1]],
-    'crud'      => ['fajl' => 'crud',      'szoveg' => 'CRUD', 'menun' => [1,1]],
+    'crud'      => ['fajl' => 'crud',      'szoveg' => 'CRUD', 'menun' => [0,1]],
+    'belep'      => ['fajl' => 'belep',     'szoveg' => '',             'menun' => [0,0]],
     'kilepes'    => ['fajl' => 'kilepes',   'szoveg' => 'Kilépés',      'menun' => [0,1]],
 ];
 
